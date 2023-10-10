191d3690b5979498f06d4665621817329710e84d
Kingofhell1 <chernyakov_99@list.ru>
2023-10-10 21:21:57 +0300

    init commit

--- src/StoreToys/Main.java
+++ src/StoreToys/Main.java
@@ -2,7 +2,7 @@ package StoreToys;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
+
 
 public class Main {
 
@@ -10,11 +10,10 @@ public class Main {
         StoreToys storeToys = new StoreToys();
         ArrayList<Toys> toys = new ArrayList<>();
         toys.add(new Toys(1, "Кукла", 20));
-        toys.add(new Toys(2, "Мяч", 50));
-        toys.add(new Toys(3, "Конструктор", 10));
-        toys.add(new Toys(4, "Машина", 40));
-        toys.add(new Toys(5, "Настольная игра", 5));
-        toys.add(new Toys(6, "Робот", 15));
+        toys.add(new Toys(2, "Конструктор", 10));
+        toys.add(new Toys(3, "Машина", 40));
+        toys.add(new Toys(4, "Настольная игра", 5));
+        toys.add(new Toys(5, "Робот", 15));
         storeToys.fileWriter(toys , 5);
 
 
